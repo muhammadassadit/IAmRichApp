66a4413d3b937ec9cea079e3d277bfe6c4c6d3b6
muhammadassadit <muhammadassadit@gmail.com>
2019-12-11 13:03:08 -0500

    Simple I Am rich app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Raja Zeb on 12/11/19.
+//  Copyright © 2019 Muhammad Assad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
